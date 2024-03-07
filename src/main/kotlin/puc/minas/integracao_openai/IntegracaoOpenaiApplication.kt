8d25ca3cf858d8dd1fce5df1eb9b8833ae42ceaa
Thiago Martins <thiagolealmartins@gmail.com>
2024-03-07 20:24:59 -0300

    primeiro commit

---
+++ src/main/kotlin/puc/minas/integracao_openai/IntegracaoOpenaiApplication.kt
@@ -0,0 +1,11 @@
+package puc.minas.integracao_openai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntegracaoOpenaiApplication
+
+fun main(args: Array<String>) {
+	runApplication<IntegracaoOpenaiApplication>(*args)
+}
---
+++ src/test/kotlin/puc/minas/integracao_openai/IntegracaoOpenaiApplicationTests.kt
@@ -0,0 +1,13 @@
+package puc.minas.integracao_openai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IntegracaoOpenaiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
